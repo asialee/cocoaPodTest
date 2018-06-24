98a10787e18af7b4d4468c722cf848533a0e5662
asia.lyz <asia.lyz@alibaba-inc.com>
2018-06-24 17:22:04 +0800

    Initial Commit

---
+++ TestLibrary/TestLibrary.h
@@ -0,0 +1,19 @@
+//
+//  TestLibrary.h
+//  TestLibrary
+//
+//  Created by liyazhou on 2018/6/24.
+//  Copyright © 2018年 liyazhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestLibrary.
+FOUNDATION_EXPORT double TestLibraryVersionNumber;
+
+//! Project version string for TestLibrary.
+FOUNDATION_EXPORT const unsigned char TestLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibrary/PublicHeader.h>
+
+
